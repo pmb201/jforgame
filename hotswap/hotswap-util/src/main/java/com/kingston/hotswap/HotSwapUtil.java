2955c64ab299b905d916b89f28caff344ea5f0b5
dengjie <963160294@qq.com>
2020-09-25 18:00:56 +0800

    tool.jar 屏蔽

--- hotswap/hotswap-util/src/main/java/com/kingston/hotswap/HotSwapUtil.java
+++ hotswap/hotswap-util/src/main/java/com/kingston/hotswap/HotSwapUtil.java
@@ -1,6 +1,5 @@
 package com.kingston.hotswap;
 
-import com.sun.tools.attach.VirtualMachine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,29 +18,30 @@ public class HotSwapUtil {
     private static Logger logger = LoggerFactory.getLogger(HotSwapUtil.class);
 
     public static String reloadClass(String path) {
-        try {
+//        try {
             // 拿到当前jvm的进程id
-            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-            VirtualMachine vm = VirtualMachine.attach(pid);
-            path = "hotswap" + File.separator + path;
-            log = "empty";
-            exception = null;
-            List<File> files = listFiles(path);
-            logger.error("热更目录[{}]，总共有{}个文件", path, files.size());
-            for (File file : files) {
-                String classPath = path + File.separator + file.getName();
-                logger.error("reload path ==" + classPath);
-                // path参数即agentmain()方法的第一个参数
-                vm.loadAgent("agent/hotswap-agent.jar", classPath);
-                logger.error("热更日志{}", log);
-                logger.error("热更异常{}", exception);
-            }
-//            return "热更的文件有 " + String.join(",", succFiles);
-            return log;
-        } catch (Throwable e) {
-            logger.error("", e);
-            return "热更失败";
-        }
+//            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+//            VirtualMachine vm = VirtualMachine.attach(pid);
+//            path = "hotswap" + File.separator + path;
+//            log = "empty";
+//            exception = null;
+//            List<File> files = listFiles(path);
+//            logger.error("热更目录[{}]，总共有{}个文件", path, files.size());
+//            for (File file : files) {
+//                String classPath = path + File.separator + file.getName();
+//                logger.error("reload path ==" + classPath);
+//                // path参数即agentmain()方法的第一个参数
+//                vm.loadAgent("agent/hotswap-agent.jar", classPath);
+//                logger.error("热更日志{}", log);
+//                logger.error("热更异常{}", exception);
+//            }
+////            return "热更的文件有 " + String.join(",", succFiles);
+//            return log;
+            return  null;
+//        } catch (Throwable e) {
+//            logger.error("", e);
+//            return "热更失败";
+//        }
     }
 
     private static List<File> listFiles(String path) {
